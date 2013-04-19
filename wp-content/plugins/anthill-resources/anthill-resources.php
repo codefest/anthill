43efd3a66fd319c414c285997aaa93c4f7a272f9
annedorko <consult@annedorko.com>
2013-04-18 21:30:01 -0700

    Folder and init file for the Resources plugin, no functionality yet

---
+++ wp-content/plugins/anthill-resources/anthill-resources.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Resources for Anthill
+Plugin URI: http://github.com/codefest/anthill
+Description: This plugin supports the Resource custom post type.
+Version: 0.1
+Author: Anne Dorko
+Author URI: http://www.annedorko.com/
+License: GPL2
+*/
