4cdd9d7e191d9bcba9f3264581211956d6ef8b26
melissacabral <melissacabral@gmail.com>
2013-05-22 14:11:47 -0700

    Changing search form code to support icon and style guide
    * moved humans.txt into the right place
    * put old copies of the DB into an archive folder for clarity

--- wp-content/themes/anthill/searchform.php
+++ wp-content/themes/anthill/searchform.php
@@ -1,15 +1,20 @@
 <?php
 /**
- * Dedicated search form with wordpress default search form code -JJ 
+ * Custom search form based on wordpress default 
+ * Supports custom icon button 
  *
  * @package WordPress
  * @subpackage Anthill
  * @since Anthill 0.1
+ * @TODO edit icon to match new custom icon solution
+ * @TODO ver 2: fancify with 'dropdown' filtering for search type -mc
  */
 ?>
 <form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
     <div><label class="screen-reader-text" for="s">Search for:</label>
         <input type="text" value="" name="s" id="s" />
-        <input type="submit" id="searchsubmit" value="Search" />
+        <button type="submit" class="btn btn-success" id="searchsubmit">
+                <i class="icon-search"></i><span class="screen-reader-text"> Search</span>
+        </button>
     </div>
 </form>
