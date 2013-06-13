d1b99badb6a22f99a198fedba2a2a2740e72308f
Joe Juette <joe.juette@gmail.com>
2013-06-13 11:37:47 -0700

    Cleaned up single.php
    Please dont hate me =)

--- wp-content/themes/anthill/single.php
+++ wp-content/themes/anthill/single.php
@@ -10,57 +10,40 @@
 <?php get_header(); ?>
 <?php get_sidebar(); ?>
 	<main>
-
-
 		<?php if ( have_posts() ) : ?>
 		<?php while ( have_posts() ) : the_post(); ?>
 		<article>
 			<?php //@TODO  beef up these loop contents! ?>
-
 			<!-- page category -->
 			<div class="category-icon"><i class="icon-magic"></i> Web</div>
-
 			<!-- title -->
 			<h1 class="entry-title"><?php the_title(); ?></h1>
-
 			<!-- Source Image -->
 			<img src="http://placekitten.com/593/250">
-
 			<!-- Source Link -->
 			<h4><a href="#">Source: Smashing Magazine</a></h4>
-
 			<!-- Author Content-->
 			<div>
 				<div>
 					<!-- Gravatar -->
 					<a href="#"><img src="http://placekitten.com/49/49"></a>
-
 					<!-- Username -->
 					<p>thoraxmcnally007</p>
-
 					<!-- date posted -->
 					<p>1 day ago</p>
-
 					<!-- Content -->
 					<div class="entry-content">
 						<?php the_content(); ?>			
 					</div>
 				</div>
-
 				<?php wp_list_comments(); ?>
-
-
-
 				<div>
 					<!-- Gravatar -->
 					<a href="#"><img src="http://placekitten.com/49/49"></a>
-
 					<!-- Username -->
 					<p>thoraxmcnally007</p>
-
 					<!-- date posted -->
 					<p>1 day ago</p>
-
 					<!-- Content -->
 					<div class="entry-content">
 						<?php the_content(); ?>			
@@ -68,24 +51,15 @@
 				</div>
 			</div>
 		</article>
-
 	<?php endwhile; ?>				
-
 <?php else : ?>
-
 	<article id="post-0" class="post no-results not-found hentry">
-
 		<h1 class="entry-title">Nothing Found</h1>
-
-
 		<div class="entry-content">
 			<p>no results were found for the requested archive. Perhaps searching will help find a related post.</p>
 			<?php get_search_form(); ?>
 		</div><!-- .entry-content -->
 	</article><!-- #post-0 -->
-
 <?php endif; ?>		
-
 </main>
-
 <?php get_footer(); ?>
